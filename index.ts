c111ae2c825c546c48bcda152bf6668a39379e4b
Thiti Yamsung <mr.thiti@icloud.com>
2025-06-10 11:05:29 +0700

    feat: add index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import 'reflect-metadata';
+import { App, Controller, Get, Injectable, Module } from 'ozean';
+
+@Injectable()
+export class CatService {
+  getCats() {
+    return "Hello cat.";
+  }
+}
+
+@Controller('/cats')
+export class CatController {
+  constructor(private readonly catService: CatService) {}
+
+  @Get()
+  findAll() {
+    return this.catService.getCats();
+  }
+}
+
+@Module({
+  controllers: [CatController],
+  providers: [CatService],
+})
+export class AppModule {}
+
+const port = 3000;
+const app = new App(AppModule).listen(port);
